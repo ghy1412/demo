eb8acf98094a51214165718aa6f293db63cc0de6
ghy141412 <1094185589@qq.com>
2019-10-16 20:45:55 +0800

    springBoot slf4j thymeleaf

--- springboot_autoconfig/src/main/java/com/bit/springboot_autoconfig/controller/HelloController.java
+++ springboot_autoconfig/src/main/java/com/bit/springboot_autoconfig/controller/HelloController.java
@@ -1,15 +1,37 @@
 package com.bit.springboot_autoconfig.controller;
 
+import com.bit.springboot_autoconfig.po.User;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.util.ArrayList;
+import java.util.Map;
+
+@Controller
 public class HelloController {
 
     @RequestMapping("/hello")
     public String sayHello(){
-        return "尼玛炸啦,";
+        return "demo";
     }
+
+
+    @RequestMapping("/thyme")
+    public String demoThymeleaf(Map<String, String> map, Model model){
+        ArrayList<User> usersList = new ArrayList<>();
+        for (int i = 0; i < 14; i++) {
+            User user = new User();
+            user.setName(i+"nc");
+            user.setAge(i+100);
+            usersList.add(user);
+        }
+        model.addAttribute("usersList",usersList);
+        map.put("user","我是demoThymeLeft");
+        return "demo";
+    }
+
 }
--- springboot_autoconfig/src/test/java/com/bit/springboot_autoconfig/SpringbootAutoconfigApplicationTests.java
+++ springboot_autoconfig/src/test/java/com/bit/springboot_autoconfig/SpringbootAutoconfigApplicationTests.java
@@ -2,6 +2,9 @@ package com.bit.springboot_autoconfig;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.ILoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -11,6 +14,8 @@ public class SpringbootAutoconfigApplicationTests {
 
     @Test
     public void contextLoads() {
-    }
+/*        Logger logger = LoggerFactory.getLogger(getClass());
+        logger.error("error的日志方式");*/
+}
 
 }
